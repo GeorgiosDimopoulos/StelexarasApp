c459afcfdfaf74fb471393ef0d42fa362ba61f92
Georgios Dimopoulos <georgdimopoulos@outlook.com.gr>
2024-09-05 17:55:53 +0200

    Add first UI Selenium Tests

---
+++ StelexarasApp.Tests/UiTests/PaidiaPageUITests.cs
@@ -0,0 +1,69 @@
+﻿namespace StelexarasApp.Tests.UiTests
+{
+    using OpenQA.Selenium;
+    using OpenQA.Selenium.Chrome;
+    using Xunit;
+
+    namespace StelexarasApp.Tests.UITests
+    {
+        public class PaidiaPageUITests : IDisposable
+        {
+            private readonly IWebDriver _driver;
+
+            public PaidiaPageUITests()
+            {
+                _driver = new ChromeDriver();
+            }
+
+            [Fact]
+            public void CreatePage_ShouldLoadSuccessfully()
+            {
+                // Arrange
+                _driver.Navigate().GoToUrl("http://localhost:5000/Paidia/Create");
+
+                // Act
+                var pageTitle = _driver.Title;
+
+                // Assert
+                Assert.Equal("Create Paidia - StelexarasApp", pageTitle);
+            }
+
+            [Fact]
+            public void IndexPage_ShouldLoadSuccessfully()
+            {
+                // Arrange
+                _driver.Navigate().GoToUrl("http://localhost:5000/Paidia/Index");
+
+                // Act
+                var pageTitle = _driver.Title;
+
+                // Assert
+                Assert.Equal("Paidia - StelexarasApp", pageTitle);
+            }
+
+            [Fact]
+            public void CreatePage_ShouldCreateNewPaidia()
+            {
+                // Arrange
+                _driver.Navigate().GoToUrl("http://localhost:5000/Paidia/Create");
+
+                // Act
+                _driver.FindElement(By.Id("Name")).SendKeys("Test Paidia");
+                _driver.FindElement(By.Id("Description")).SendKeys("Test Description");
+                _driver.FindElement(By.CssSelector("input[type='submit']")).Click();
+
+                // Assert
+                var successMessage = _driver.FindElement(By.CssSelector(".alert-success")).Text;
+                Assert.Contains("has been created", successMessage);
+            }
+
+            public void Dispose()
+            {
+                _driver.Quit();
+                _driver.Dispose();
+            }
+        }
+    }
+
+
+}
---
+++ StelexarasApp.Tests/UiTests/TeamsPageUITests.cs
@@ -0,0 +1,47 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Xunit;
+
+namespace StelexarasApp.Tests.UiTests
+{   
+    namespace StelexarasApp.Tests.UITests
+    {
+        public class TeamsPageUITests
+        {
+            [Fact]
+            public void CreatePage_ShouldLoadSuccessfully()
+            {
+                // Arrange
+                using var driver = new ChromeDriver();
+                driver.Navigate().GoToUrl("http://localhost:5000/Teams/Create");
+
+                // Act
+                var pageTitle = driver.Title;
+
+                // Assert
+                Assert.Equal("Create Team - StelexarasApp", pageTitle);
+
+                // Clean up
+                driver.Quit();
+            }
+
+            [Fact]
+            public void IndexPage_ShouldLoadSuccessfully()
+            {
+                // Arrange
+                using var driver = new ChromeDriver();
+                driver.Navigate().GoToUrl("http://localhost:5000/Teams/Index");
+
+                // Act
+                var pageTitle = driver.Title;
+
+                // Assert
+                Assert.Equal("Teams - StelexarasApp", pageTitle);
+
+                // Clean up
+                driver.Quit();
+            }
+        }
+    }
+
+}
