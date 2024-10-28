212f288b5ad4814a9b0fa365acd261727badbded
Georgios Dimopoulos <georgdimopoulos@outlook.com.gr>
2024-10-28 19:57:00 +0100

    Add StelexosQueryParameters for common queries params

---
+++ StelexarasApp.API/StelexosQueryParameters.cs
@@ -0,0 +1,18 @@
+﻿using StelexarasApp.DataAccess.Models.Atoma.Staff;
+
+namespace StelexarasApp.API
+{
+    public class StelexosQueryParameters
+    {
+        public Guid? Id { get; set; }               // Filter by unique identifier
+        public string? FullName { get; set; }        // Filter by name
+        public Thesi? Thesi { get; set; }            // Filter by position enum
+        public Guid? KoinotitaId { get; set; }       // Filter by associated Koinotita
+        public Guid? TomeasId { get; set; }          // Filter by associated Tomeas
+
+        // Pagination
+        public int PageNumber { get; set; } = 1;     // Page number
+        public int PageSize { get; set; } = 10;      // Results per page
+    }
+
+}
