18c2ac00d1b17ca49492e45deadbaffcd41fc0df
Georgios Dimopoulos <georgdimopoulos@outlook.com.gr>
2025-06-07 14:25:00 +0200

    create Diagrams folder and initial drawio Diagram

--- StelexarasApp.Library/Models/Atoma/Staff/Ekpaideutis.cs
+++ StelexarasApp.Library/Models/Atoma/Staff/Ekpaideutis.cs
@@ -9,8 +9,6 @@ namespace StelexarasApp.Library.Models.Atoma.Staff
         public string FullName { get; set; } = string.Empty;
         public int Age { get; set; }
         public Sex Sex { get; set; }
-
-        [Required]
         public string Tel { get; set; } = string.Empty;
         public Thesi Thesi { get; set; } = Thesi.Ekpaideutis;
         public string? XwrosName { get; set; }
