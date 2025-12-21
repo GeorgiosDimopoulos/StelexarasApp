dabc88ac3d36e3a865534256e83bde7cc679f48b
Georgios Dimopoulos <georgdimopoulos@outlook.com.gr>
2025-12-21 14:56:33 +0100

    feat: start adding refit iapis in the UI

---
+++ StelexarasApp.Application/ApiClients/IKoinotitesApi.cs
@@ -0,0 +1,25 @@
+﻿using Refit;
+using StelexarasApp.Library.Dtos.Domi;
+
+namespace StelexarasApp.Application.ApiClients;
+
+public interface IKoinotitesApi
+{
+    [Get("/Koinotites/Koinotites")]
+    Task<List<KoinotitaResponse>> GetKoinotitesAsync();
+
+    [Get("/Koinotites/Koinotites/{tomeaId}")]
+    Task<List<KoinotitaResponse>> GetKoinotitesByTomeaAsync(int tomeaId);
+
+    [Get("/Koinotites/Koinotita/{name}")]
+    Task<KoinotitaResponse> GetKoinotita(string name);
+
+    [Post("/Koinotites/Koinotita")]
+    Task<bool> PostKoinotita([Body] CreateKoinotitaRequest dto);
+
+    [Delete("/Koinotites/Koinotita/{id}")]
+    Task<bool> DeleteKoinotita(int id);
+
+    [Put("/Koinotites/Koinotita/{id}")]
+    Task<bool> PutKoinotita(int id, [Body] UpdateKoinotitaRequest dto);
+}
---
+++ StelexarasApp.Application/ApiClients/ISkinesApi.cs
@@ -0,0 +1,29 @@
+﻿using Refit;
+using StelexarasApp.Library.Dtos.Domi;
+using StelexarasApp.Library.QueryParameters.Domi;
+
+namespace StelexarasApp.Application.ApiClients;
+
+public interface ISkinesApi
+{
+    [Get("/Skines/Skines")]
+    Task<List<SkiniResponse>> GetSkinesAsync([Query] SkiniQueryParameters skinesQueryParameters);
+
+    [Get("Skines//Skini/{name}")]
+    Task<SkiniResponse> GetSkini(string name, [Query] SkiniQueryParameters skiniQueryParameters);
+
+    [Post("Skines//Skini")]
+    Task<SkiniResponse> PostSkini([Query] CreateSkiniRequest skiniDto);
+
+    [Delete("Skines//Skini/{id}")]
+    Task DeleteSkini(int id);
+
+    [Put("Skines//Skini/{id}")]
+    Task PutSkini(int id, [Body] UpdateSkiniRequest skiniDto);
+
+    [Get("/Skines/Skines/{koinotitaName}")]
+    Task<List<SkiniResponse>> GetSkinesByKoinotitaAsync(string koinotitaName);
+
+    [Get("/Skines/SkinesEkpaideuomenon")]
+    Task<List<SkiniResponse>> GetSkinesEkpaideuomenonAsync();
+}
