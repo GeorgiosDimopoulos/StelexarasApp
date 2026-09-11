8ad930c23b34afee2b10ac905b5abbde99f94758
Georgios Dimopoulos <georgdimopoulos@outlook.com.gr>
2026-09-11 13:03:47 +0200

    fix: remove KoinotarxisId from Omadarxis domain model

---
+++ StelexarasApp.DataAccess/Migrations/AppDb/20260911105619_CheckKoinotarxisIdInOmadarxes.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StelexarasApp.DataAccess.Migrations.AppDb
+{
+    /// <inheritdoc />
+    public partial class CheckKoinotarxisIdInOmadarxes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Omadarxes_Koinotarxes_KoinotarxisId",
+                table: "Omadarxes");
+
+            migrationBuilder.DropIndex(        
+                name: "IX_Omadarxes_KoinotarxisId",
+                table: "Omadarxes");
+
+            migrationBuilder.DropColumn(
+                name: "KoinotarxisId",
+                table: "Omadarxes");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
